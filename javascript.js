63e4756ebbf90c344a8e0a99d955f27d09f1a27d
Rachael <rdougherty031@gmail.com>
2019-04-08 16:36:14 -0400

    nav bar & resume

--- javascript.js
+++ javascript.js
@@ -13,35 +13,35 @@ anime({
     // loop: true
 });
 // Wrap every letter in a span
-setTimeout(header, 1000);
-function header() {
-    $(".letters").css("visibility","visible");
-    $('.ml1 .letters').each(function(){
-        $(this).html($(this).text().replace(/([^\x00-\x80]|\w)/g, "<span class='letter'>$&</span>"));
-    });
+// setTimeout(header, 1000);
+// function header() {
+//     $(".letters").css("visibility","visible");
+//     $('.ml1 .letters').each(function(){
+//         $(this).html($(this).text().replace(/([^\x00-\x80]|\w)/g, "<span class='letter'>$&</span>"));
+//     });
     
-    anime.timeline({loop: false})
-        .add({
-        targets: '.ml1 .letter',
-        scale: [0.3,1],
-        opacity: [0,1],
-        translateZ: 0,
-        easing: "easeOutExpo",
-        duration: 1000,
-        delay: function(el, i) {
-            return 70 * (i+1)
-        }
-        }).add({
-        targets: '.ml1 .line',
-        scaleX: [0,1],
-        opacity: [0.5,1],
-        easing: "easeOutExpo",
-        duration: 1000,
-        offset: '-=875',
-        delay: function(el, i, l) {
-            return 80 * (l - i);
-        }
-        });
+//     anime.timeline({loop: false})
+//         .add({
+//         targets: '.ml1 .letter',
+//         scale: [0.3,1],
+//         opacity: [0,1],
+//         translateZ: 0,
+//         easing: "easeOutExpo",
+//         duration: 1000,
+//         delay: function(el, i) {
+//             return 70 * (i+1)
+//         }
+//         }).add({
+//         targets: '.ml1 .line',
+//         scaleX: [0,1],
+//         opacity: [0.5,1],
+//         easing: "easeOutExpo",
+//         duration: 1000,
+//         offset: '-=875',
+//         delay: function(el, i, l) {
+//             return 80 * (l - i);
+//         }
+//         });
         // .add({
         // targets: '.ml1',
         // opacity: 0,
@@ -49,4 +49,4 @@ function header() {
         // easing: "easeOutExpo",
         // delay: 1000
         // });
-    }
+    // }
