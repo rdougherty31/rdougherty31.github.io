5d72286badf3377b41584a09308dda36f287f1f7
Rachael <rdougherty031@gmail.com>
2019-08-08 15:18:37 -0400

    removed initial bg animation

--- javascript.js
+++ javascript.js
@@ -1,20 +1,20 @@
-$( document ).ready(function() {
-    function portfolioPage() {
-        $(".headshot").css("display","none");
-        $(".portfolio").css("display","block");
-    }
-    $("#portfolio").click(function() {
-        portfolioPage();
-    });
+// $( document ).ready(function() {
+//     function portfolioPage() {
+//         $(".headshot").css("display","none");
+//         $(".portfolio").css("display","block");
+//     }
+//     $("#portfolio").click(function() {
+//         portfolioPage();
+//     });
 
-    anime({
-        targets: '.box',
-        rotate: {
-            value: '1turn',
-            easing: 'easeInOutSine'
-        },
-        opacity: '0.5',
-        delay: function(el, i, l){return i*50}
-        // loop: true
-    });
-});
+//     anime({
+//         targets: '.box',
+//         rotate: {
+//             value: '1turn',
+//             easing: 'easeInOutSine'
+//         },
+//         opacity: '0.5',
+//         delay: function(el, i, l){return i*50}
+//         // loop: true
+//     });
+// });
